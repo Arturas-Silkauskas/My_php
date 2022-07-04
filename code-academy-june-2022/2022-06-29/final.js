e69b1090e281fe460d27c093e75c50a1bebd6183
Arturas <arturas.web.dev@gmail.com>
2022-07-04 19:42:15 +0300

    Final js task version

--- code-academy-june-2022/2022-06-29/final.js
+++ code-academy-june-2022/2022-06-29/final.js
@@ -6,69 +6,69 @@ document.querySelector('.first')?.addEventListener('click', function() {
 
 // SECOND TASK
 
-let inp = document.querySelector('#inp1');
-let btn1 = document.querySelector('#btn1')
-let btn2 = document.querySelector('#btn2')
-let btn3 = document.querySelector('#btn3')
-let btn4 = document.querySelector('#btn4')
+let inpt = document.querySelector('#input1');
+let btn1 = document.querySelector('#button1')
+let btn2 = document.querySelector('#button2')
+let btn3 = document.querySelector('#button3')
+let btn4 = document.querySelector('#button4')
 
 btn1?.addEventListener('click', function() {
-    inp.value = inp.value.toUpperCase();
+    inpt.value = inpt.value.toUpperCase();
 })
 btn2?.addEventListener('click', function() {
-    inp.value = inp.value.toLowerCase();
+    inpt.value = inpt.value.toLowerCase();
 })
 btn3?.addEventListener('click', function() {
-    inp.value = inp.value[0].toUpperCase() + inp.value.slice(1);
+    inpt.value = inpt.value[0].toUpperCase() + inpt.value.slice(1);
 })
 btn4?.addEventListener('click', function() {
-    inp.value = inp.value[0].toLowerCase() + inp.value.slice(1);
+    inpt.value = inpt.value[0].toLowerCase() + inpt.value.slice(1);
 })
 
 // THIRD TASK
 
-let idEmail = document.querySelector('#email');
-let idPhone = document.querySelector('#phone');
-let btnSave = document.querySelector('#btnSave');
-let emailCheck = document.querySelector('#emailCheck');
-let phoneCheck = document.querySelector('#phoneCheck');
+let email = document.querySelector('#email');
+let phone = document.querySelector('#phone');
+let btnSave = document.querySelector('#saveBtn');
+let checkEmail = document.querySelector('#checkemail');
+let checkPhone = document.querySelector('#checkphone');
 
 btnSave?.addEventListener('click', function() {
-    if (!idEmail.value.includes('@') && idPhone.value == "") {
-        idEmail.classList.add('is-invalid');
-        emailCheck.style.display = 'block';
-        idPhone.classList.add('is-invalid');
-        phoneCheck.style.display = 'block';
-    }else if (!idEmail.value.includes('@')) {
-        idEmail.classList.add('is-invalid');
-        emailCheck.style.display = 'block';
-        idPhone.classList.remove('is-invalid');
-        phoneCheck.style.display = 'none';
-    }else if (idPhone.value == "") {
-        idPhone.classList.add('is-invalid');
-        phoneCheck.style.display = 'block';
-        idEmail.classList.remove('is-invalid');
-        emailCheck.style.display = 'none';
+    if (!email.value.includes('@') && phone.value == "") {
+        email.classList.add('is-invalid');
+        checkEmail.style.display = 'block';
+        phone.classList.add('is-invalid');
+        checkPhone.style.display = 'block';
+    }else if (!email.value.includes('@')) {
+        email.classList.add('is-invalid');
+        checkEmail.style.display = 'block';
+        phone.classList.remove('is-invalid');
+        checkPhone.style.display = 'none';
+    }else if (phone.value == "") {
+        phone.classList.add('is-invalid');
+        checkPhone.style.display = 'block';
+        email.classList.remove('is-invalid');
+        checkEmail.style.display = 'none';
     }
 });
 
 // FOURTH TASK
 
-let btnBlock = document.querySelector('#block');
-let btnUnblock = document.querySelector('#unblock');
-let inpBlockUnblock = document.querySelector('#inpBlockUnblock');
+let blockButton = document.querySelector('#blockBtn');
+let unblockButton = document.querySelector('#unblockBtn');
+let inputBlockUnblock = document.querySelector('#blockUnblock');
 
-btnBlock?.addEventListener('click', function() {
-    inpBlockUnblock?.setAttribute('readonly','');
+blockButton?.addEventListener('click', function() {
+    inputBlockUnblock?.setAttribute('readonly','');
 });
 
-btnUnblock?.addEventListener('click', function() {
-    inpBlockUnblock?.removeAttribute('readonly');
+unblockButton?.addEventListener('click', function() {
+    inputBlockUnblock?.removeAttribute('readonly');
 });
 
 // FIFTH TASK
 
-let img = document.querySelector('#image');
+let img = document.querySelector('#imgMonkeys');
 
 img?.addEventListener('mouseenter', function() {
     img.setAttribute('src', 'https://i.imgur.com/PLDVxza.jpg');
@@ -79,23 +79,21 @@ img?.addEventListener('mouseleave', function() {
 
 // SIXTH TASK
 
-let resetAll = document.querySelector('#resetAll')
+let reset = document.querySelector('#reset')
 
 let pointer = document.querySelector('#pointer');
-let textCursor = document.querySelector('#textCursor');
+let text = document.querySelector('#text');
 let copy = document.querySelector('#copy');
 let help = document.querySelector('#help');
 let crosshair = document.querySelector('#crosshair');
 
-let changeText = document.querySelector('#text');
+let changeText = document.querySelector('#randomText');
 let redColor = document.querySelector('#redColor');
 let greenColor = document.querySelector('#greenColor');
 let blueColor = document.querySelector('#blueColor');
 let redBorder = document.querySelector('#redBorder');
 let greenBorder = document.querySelector('#greenBorder');
 let blueBorder = document.querySelector('#blueBorder');
-
-// COLORS
 
 redColor?.addEventListener('click', function(e) {
     e.preventDefault();
@@ -110,28 +108,28 @@ blueColor?.addEventListener('click', function(e) {
     changeText.style.color = 'blue';
 });
 
-// BORDERS
+
 
 redBorder?.addEventListener('click', function(e) {
     e.preventDefault();
-    changeText.style.outline = '2px solid red';
+    changeText.style.border = '2px solid red';
 });
 greenBorder?.addEventListener('click', function(e) {
     e.preventDefault();
-    changeText.style.outline = '2px solid green';
+    changeText.style.border = '2px solid green';
 });
 blueBorder?.addEventListener('click', function(e) {
     e.preventDefault();
-    changeText.style.outline = '2px solid blue';
+    changeText.style.border = '2px solid blue';
 });
 
-// CURSORS
+
 
 pointer?.addEventListener('click', function(e) {
     e.preventDefault();
     document.body.style.cursor = 'pointer';
 });
-textCursor?.addEventListener('click', function(e) {
+text?.addEventListener('click', function(e) {
     e.preventDefault();
     document.body.style.cursor = 'text';
 });
@@ -148,9 +146,9 @@ crosshair?.addEventListener('click', function(e) {
     document.body.style.cursor = 'crosshair';
 });
 
-resetAll?.addEventListener('click', function(e) {
+reset?.addEventListener('click', function(e) {
     e.preventDefault();
     changeText.style.color = 'black';
-    changeText.style.outline = 'none';
+    changeText.style.border = 'none';
     document.body.style.cursor = 'auto';
 })
